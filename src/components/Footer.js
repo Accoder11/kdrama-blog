e2114980b6f4eb7cf2ae42b3c707231767db37f3
Alma Cleto <emscleto11@gmail.com>
2023-02-03 03:06:35 +0800

    create changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container fluid className='text-center text-lg-start bg-warning text-muted'>
         <div>
           © 2023 Copyright: 
-          <a className='footer-link fw-bold' href='https://accoder11.github.io/react-playground' target="_blank" rel="noopener noreferrer"> www.emcleto@kodegoPh.com
+          <a className='footer-link fw-bold' href='https://accoder11.github.io/kdrama-blog/' target="_blank" rel="noopener noreferrer"> www.emcleto@kodegoPh.com
           </a>
         </div>
       </Container>

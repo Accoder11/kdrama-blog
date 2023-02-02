cf355511c7a28a56a504974a00e09db8c639e193
Alma Cleto <emscleto11@gmail.com>
2023-02-02 21:16:34 +0800

    updated

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Container from 'react-bootstrap/Container';
-import { Outlet, Link } from "react-router-dom";
 
-const Blog = () => {
+const Blog1 = () => {
     return (
     <CardGroup >
-        <Link to="/blogs"><h1>Blog</h1></Link>
     <Card className="all-products">
     <Card.Img variant="top" className="card-img col-md-5 mx-2 d-block w-100 fluid" src="https://placeimg.com/640/480/animals" />
       <Card.Body className='mb-0 pb-0'>
@@ -39,14 +37,14 @@ When you develop a mockup page or backend API is not ready for data fetching and
     );
   }
 
-const Blogs = () => {
+const Blog = () => {
     return (
       <Container className="p-3 my-5">
-            <Blogs></Blogs>
-            <Blogs></Blogs>
-            <Blogs></Blogs>
+            <Blog1></Blog1>
+            <Blog1></Blog1>
+            <Blog1></Blog1>
       </Container>
     );
   }
 
-  export default Blogs;
+  export default Blog;
